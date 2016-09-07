246d1477497c5b5b3c84025e61c4da6fb583247b
chen <179023084@qq.com>
2016-09-07 14:08:33 +0800

    add form

---
+++ Basic8/demo1.php
@@ -0,0 +1,5 @@
+<form action="demo2.php" method="post">
+	Name: <input type="text" name="name" /><br />
+	E-mail: <input type="text" name="email" /><br />
+	<input type="submit" value="提交" />
+</form>
---
+++ Basic8/demo3.php
@@ -0,0 +1,5 @@
+<form action="demo4.php" method="get">
+	Name: <input type="text" name="name" /><br />
+	E-mail: <input type="text" name="email" /><br />
+	<input type="submit" value="提交" />
+</form>
